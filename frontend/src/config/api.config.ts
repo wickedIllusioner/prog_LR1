ae469febf7cd1d3325bb23b628aa4f79df1c9da1
Artyom Veselov <xxxvanyalive228xxx@gmail.com>
2026-05-27 22:58:11 +0700

    fix(frontend): fix API config for auth endpoint

--- frontend/src/config/api.config.ts
+++ frontend/src/config/api.config.ts
@@ -3,7 +3,7 @@ export const SERVER_URL = process.env.NEXT_PUBLIC_API_URL || process.env.SERVER_
 export const API_URL = {
 	root: (url = '') => `${url ? url : ''}`,
 
-	auth: (url = '') => API_URL.root(`/auth`),
+	auth: (url = '') => API_URL.root(`/auth${url ? `/${url}` : ''}`),
 	drivers: (url = '') => API_URL.root(`/drivers/${url}`),
 	vehicles: (url = '') => API_URL.root(`/vehicles/${url}`),
 	incidents: (url = '') => API_URL.root(`/incidents/${url}`),
